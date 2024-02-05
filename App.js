8017fc78ca02383557f23807ca09486b1dffc383
Mansurol <mansurolislam@gmail.com>
2024-02-05 09:45:23 +0600

    update

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
-import { View, Text } from "react-native";
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { View } from "react-native";
 import StackNav from "./Navigation/StackNav";
 import { Provider } from "react-redux";
 import Store from "./Redux/Store";
